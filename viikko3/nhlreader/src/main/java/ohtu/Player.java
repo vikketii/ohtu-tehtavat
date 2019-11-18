76ba4ec248f7af88c3deba5bafeb715970515795
Vikke Tiirola <vikke.tiirola@gmail.com>
2019-11-18 11:06:53 +0200

    week 3 exercise 1 done

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,57 @@
+
+package ohtu;
+
+public class Player {
+    private String name;
+    private int goals;
+    private int assists;
+    private String team;
+    private String nationality;
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        String info = name + " team " + team + " goals " + goals + " assists " + assists;
+        return info;
+    }
+      
+}
