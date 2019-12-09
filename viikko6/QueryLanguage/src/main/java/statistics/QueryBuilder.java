bd66a3445108daf308ad0bdb66a537bf1a1d245d
Vikke Tiirola <vikke.tiirola@gmail.com>
2019-12-09 20:59:35 +0200

    week 6 exercise 4 done

---
+++ viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -0,0 +1,30 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class QueryBuilder {
+    Matcher matcher;
+
+    public QueryBuilder() {
+        this.matcher = new All();
+    }
+
+    public Matcher build() {
+        return matcher;
+    }
+
+    public QueryBuilder playsIn(String team) {
+        this.matcher = new And(this.matcher, new PlaysIn(team));
+        return this;
+    }
+
+    public QueryBuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(this.matcher, new HasAtLeast(value, category));
+        return this;
+    }
+
+    public QueryBuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(this.matcher, new HasFewerThan(value, category));
+        return this;
+    }
+}
