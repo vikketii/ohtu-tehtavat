a4926780bcebed442494d7a8c7d18b944fad92cf
Vikke Tiirola <vikke.tiirola@gmail.com>
2019-12-09 20:35:29 +0200

    week 6 exercise 3 done

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -12,10 +12,13 @@ public class Main {
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
 
         Matcher m = new And(
-                new HasFewerThan(1, "goals"),
-                new PlaysIn("NYR")
+                new HasAtLeast(20, "points"),
+                new Or(
+                        new PlaysIn("NYR"),
+                        new PlaysIn("NYI"),
+                        new PlaysIn("NJD")
+                )
         );
-
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
